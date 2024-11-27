9bff5cc7dc8b020a65e4a02ed8ea4f2cc6e7d874
Luis Plata <www.luisplata@gmail.com>
2024-11-26 22:41:25 -0500

    +++++

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PeryLoth.com---Java---Selenium
 Robot para probar pagina web personal, ejemplo para demostrar dominio en el tema
+
+correr pruebas locales Chrome
+`mvn clean test -Dbrowser=chrome -DchromeDriverPath="/usr/local/bin/chromedriver" -DchromePath="/usr/bin/google-chrome"`
+
+correr pruebas locales firefox
+`mvn clean test -Dbrowser=firefox -DfirefoxPath="/usr/bin/firefox"`
--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -17,25 +17,31 @@ public class BaseTest {
     @Parameters("browser")
     public void setUp(String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
+            String chromeDriverPath = System.getProperty("chromeDriverPath", "/path/to/local/chromedriver");
+            String chromeBinaryPath = System.getProperty("chromePath", "/path/to/local/google-chrome");
+
             if (System.getProperty("os.name").toLowerCase().contains("win")) {
                 System.setProperty("webdriver.chrome.driver", "D:\\QA_Automatiozacion\\chromedriver-win64\\chromedriver.exe");
             } else {
-                System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+                System.setProperty("webdriver.chrome.driver", chromeDriverPath);
             }
             ChromeOptions options = new ChromeOptions();
+            options.setBinary(chromeBinaryPath);
             options.addArguments("--headless"); // Modo sin interfaz gráfica
             options.addArguments("--no-sandbox"); // Evita problemas de permisos en contenedores
             options.addArguments("--disable-dev-shm-usage"); // Usa /tmp para almacenamiento compartido
             options.addArguments("--remote-allow-origins=*"); // Permite todas las conexiones remotas
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("firefox")) {
+            String firefoxBinaryPath = System.getProperty("firefoxPath", "/path/to/local/firefox");
             if (System.getProperty("os.name").toLowerCase().contains("win")) {
                 System.setProperty("webdriver.gecko.driver", "D:\\QA_Automatiozacion\\geckodriver-v0.35.0-win64\\geckodriver.exe");
             } else {
-                System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+                System.setProperty("webdriver.gecko.driver", firefoxBinaryPath);
             }
             FirefoxOptions options = new FirefoxOptions();
             options.addArguments("--headless"); // Modo sin interfaz gráfica
+            options.setBinary(firefoxBinaryPath);
             driver = new FirefoxDriver(options);
         }
         driver.manage().window().maximize();
