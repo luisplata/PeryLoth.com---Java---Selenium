8d4b51e5a340c706fe05cbb5323bbbdbe06581c9
Luis Plata <www.luisplata@gmail.com>
2024-11-26 22:54:58 -0500

    ++++++

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 Robot para probar pagina web personal, ejemplo para demostrar dominio en el tema
 
 correr pruebas locales Chrome
-`mvn clean test -Dbrowser=chrome -DchromeDriverPath="/usr/local/bin/chromedriver" -DchromePath="/usr/bin/google-chrome"`
+```
+mvn clean test -Dbrowser=chrome -DchromeDriverPath="D:\\QA_Automatiozacion\\chromedriver-win64\\chromedriver.exe" -DchromeBinaryPath="C:\Program Files\Google\Chrome\Application\\chrome.exe"
+```
 
 correr pruebas locales firefox
-`mvn clean test -Dbrowser=firefox -DfirefoxPath="/usr/bin/firefox"`
+```
+mvn clean test -Dbrowser=firefox -DfirefoxPath="C:\\Program Files\\Mozilla Firefox\\firefox.exe" -DfirefoxDriverPath="D:\\QA_Automatiozacion\\geckodriver-v0.35.0-win64\\geckodriver.exe"
+```
--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -18,31 +18,19 @@ public class BaseTest {
     public void setUp(String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
             String chromeDriverPath = System.getProperty("chromeDriverPath", "/path/to/local/chromedriver");
-            String chromeBinaryPath = System.getProperty("chromePath", "/path/to/local/google-chrome");
-
-            if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                System.setProperty("webdriver.chrome.driver", "D:\\QA_Automatiozacion\\chromedriver-win64\\chromedriver.exe");
-            } else {
-                System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-            }
+            String chromeBinaryPath = System.getProperty("chromeBinaryPath", "/path/to/local/google-chrome");
+            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
             ChromeOptions options = new ChromeOptions();
             options.setBinary(chromeBinaryPath);
-            options.addArguments("--headless"); // Modo sin interfaz gráfica
-            options.addArguments("--no-sandbox"); // Evita problemas de permisos en contenedores
-            options.addArguments("--disable-dev-shm-usage"); // Usa /tmp para almacenamiento compartido
-            options.addArguments("--remote-allow-origins=*"); // Permite todas las conexiones remotas
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("firefox")) {
             String firefoxBinaryPath = System.getProperty("firefoxPath", "/path/to/local/firefox");
-            if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                System.setProperty("webdriver.gecko.driver", "D:\\QA_Automatiozacion\\geckodriver-v0.35.0-win64\\geckodriver.exe");
-            } else {
-                System.setProperty("webdriver.gecko.driver", firefoxBinaryPath);
-            }
+            String  firefoxDriverPath = System.getProperty("firefoxDriverPath", "/path/to/local/firefox");
+            System.setProperty("webdriver.gecko.driver",firefoxDriverPath);
             FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--headless"); // Modo sin interfaz gráfica
             options.setBinary(firefoxBinaryPath);
             driver = new FirefoxDriver(options);
+
         }
         driver.manage().window().maximize();
     }
